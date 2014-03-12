1f2f1b1d8d189711f664edac2265f77d57d5793b
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-03-12 16:13:11 -0700

    Added elc.js

---
+++ elc-test.js
@@ -0,0 +1,32 @@
+﻿/*global elc*/
+var routeLocator = new elc.RouteLocator();
+routeLocator.getRouteList(function (list) {
+	console.debug(list);
+});
+
+routeLocator.findRouteLocations({
+	locations: [
+		new elc.RouteLocation({
+			Route: "005",
+			Arm: 0,
+		})
+	],
+	referenceDate: new Date("12/31/2013"),
+	successHandler: function (routeLocations) {
+		console.log(routeLocations);
+	}
+});
+
+routeLocator.findNearestRouteLocations({
+	//useCors: useCors,
+	coordinates: [1083893.182, 111526.885],
+	referenceDate: new Date("12/31/2011"),
+	searchRadius: 1,
+	inSR: 2927,
+	successHandler: function (data) {
+		console.log(data);
+	},
+	errorHandler: function (error) {
+		console.error(error);
+	}
+});
