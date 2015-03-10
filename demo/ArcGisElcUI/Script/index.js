072d1825095ac84e60265371cd3832334269e52e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-10 16:14:05 -0700

    Added UI and demo page

---
+++ demo/ArcGisElcUI/Script/index.js
@@ -0,0 +1,13 @@
+﻿/*global require*/
+require(["esri/map", "elc-ui"], function (esriMap, ElcUI) {
+	var map, elcUI;
+
+	elcUI = new ElcUI(document.getElementById("elcUI"));
+
+	map = new esriMap("map", {
+		basemap: "gray",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
+	});
+});
