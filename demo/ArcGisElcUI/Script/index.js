2b97f5bafc43345c0faa2418b64d34843233551d
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-10 17:35:51 -0700

    Form submission triggers custom event

--- demo/ArcGisElcUI/Script/index.js
+++ demo/ArcGisElcUI/Script/index.js
@@ -4,6 +4,10 @@ require(["esri/map", "elc-ui"], function (esriMap, ElcUI) {
 
 	elcUI = new ElcUI(document.getElementById("elcUI"));
 
+	elcUI.root.addEventListener('find-route-location-submit', function (e) {
+		console.log(e.detail);
+	});
+
 	map = new esriMap("map", {
 		basemap: "gray",
 		center: [-120.80566406246835, 47.41322033015946],
