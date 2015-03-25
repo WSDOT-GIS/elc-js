813a7b4f4adbbeb06abf09bf3ec99e56865d420d
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-25 13:40:40 -0700

    ArcGisElcUI now defined with dojo/_base/declare.

--- elc/elc-ui/arcgis-elc-ui.js
+++ elc/elc-ui/arcgis-elc-ui.js
@@ -1,5 +1,6 @@
 ﻿/*global define*/
 define([
+	"dojo/_base/declare",
 	"dojo/on",
 	"esri/map",
 	"esri/graphic",
@@ -8,7 +9,7 @@ define([
 	"esri/InfoTemplate",
 	"elc/elc-ui/main",
 	"elc"
-], function (on, esriMap, Graphic, geometryJsonUtils, FeatureLayer, InfoTemplate, ElcUI, Elc) {
+], function (declare, on, esriMap, Graphic, geometryJsonUtils, FeatureLayer, InfoTemplate, ElcUI, Elc) {
 	var elcUI, routeLocator, pointResultsLayer, lineResultsLayer;
 
 	/**
@@ -53,136 +54,137 @@ define([
 		return dl;
 	}
 
-	function ArcGisBufferUI(domNode) {
+	var ArcGisElcUI = declare(null, {
+		constructor: function (domNode) {
 
-		var self = this;
+			var self = this;
 
-		/**
-		 * Adds ELC results to feature layers.
-		 */
-		function addResultsToMap(elcResults) {
-			if (elcResults && Array.isArray(elcResults)) {
-				pointResultsLayer.suspend();
-				lineResultsLayer.suspend();
-				elcResults.forEach(function (routeLocation) {
-					var graphic = routeLocationToGraphic(routeLocation);
-					if (graphic) {
-						if (graphic.geometry.type === "point") {
-							pointResultsLayer.add(graphic);
-						} else if (graphic.geometry.type === "polyline") {
-							lineResultsLayer.add(graphic);
-						} else {
-							console.warn("Unexpected geometry type", graphic);
+			/**
+			 * Adds ELC results to feature layers.
+			 */
+			function addResultsToMap(elcResults) {
+				if (elcResults && Array.isArray(elcResults)) {
+					pointResultsLayer.suspend();
+					lineResultsLayer.suspend();
+					elcResults.forEach(function (routeLocation) {
+						var graphic = routeLocationToGraphic(routeLocation);
+						if (graphic) {
+							if (graphic.geometry.type === "point") {
+								pointResultsLayer.add(graphic);
+							} else if (graphic.geometry.type === "polyline") {
+								lineResultsLayer.add(graphic);
+							} else {
+								console.warn("Unexpected geometry type", graphic);
+							}
 						}
-					}
-				});
-				pointResultsLayer.resume();
-				lineResultsLayer.resume();
-			}
-		}
-
-		routeLocator = new Elc.RouteLocator();
-
-		elcUI = new ElcUI(domNode);
-
-		elcUI.root.addEventListener('find-route-location-submit', function (e) {
-			var locations = [new Elc.RouteLocation(e.detail)];
-			routeLocator.findRouteLocations({
-				locations: locations,
-				outSR: 3857,
-				successHandler: addResultsToMap,
-				errorHandler: function (error) {
-					console.error("find route location error", error);
+					});
+					pointResultsLayer.resume();
+					lineResultsLayer.resume();
 				}
-			});
-		});
+			}
 
-		elcUI.root.addEventListener('find-nearest-route-location-submit', function (e) {
-			// Setup map click event. This should only occur ONCE.
-			on.once(self.map, "click", function (mapEvt) {
-				var mapPoint = mapEvt.mapPoint;
-				routeLocator.findNearestRouteLocations({
-					coordinates: [mapPoint.x, mapPoint.y],
-					searchRadius: e.detail.radius,
-					inSR: mapPoint.spatialReference.wkid,
-					outSR: mapPoint.spatialReference.wkid,
-					referenceDate: new Date(),
+			routeLocator = new Elc.RouteLocator();
+
+			elcUI = new ElcUI(domNode);
+
+			elcUI.root.addEventListener('find-route-location-submit', function (e) {
+				var locations = [new Elc.RouteLocation(e.detail)];
+				routeLocator.findRouteLocations({
+					locations: locations,
+					outSR: 3857,
 					successHandler: addResultsToMap,
 					errorHandler: function (error) {
-						console.error(error);
+						console.error("find route location error", error);
 					}
 				});
 			});
-		});
 
-		var infoTemplate = new InfoTemplate("Route Location", graphicToHtml);
+			elcUI.root.addEventListener('find-nearest-route-location-submit', function (e) {
+				// Setup map click event. This should only occur ONCE.
+				on.once(self.map, "click", function (mapEvt) {
+					var mapPoint = mapEvt.mapPoint;
+					routeLocator.findNearestRouteLocations({
+						coordinates: [mapPoint.x, mapPoint.y],
+						searchRadius: e.detail.radius,
+						inSR: mapPoint.spatialReference.wkid,
+						outSR: mapPoint.spatialReference.wkid,
+						referenceDate: new Date(),
+						successHandler: addResultsToMap,
+						errorHandler: function (error) {
+							console.error(error);
+						}
+					});
+				});
+			});
 
-		var elcFields = [
-				{ name: "Id", type: "esriFieldTypeInteger", alias: "ID" },
-				{ name: "Route", type: "esriFieldTypeString" },
-				{ name: "Decrease", type: "esriFieldTypeSmallInteger" },
-				{ name: "Arm", type: "esriFieldTypeDouble", alias: "ARM" },
-				{ name: "Srmp", type: "esriFieldTypeDouble", alias: "SRMP" },
-				{ name: "Back", type: "esriFieldTypeSmallInteger" },
-				{ name: "ReferenceDate", type: "esriFieldTypeDate", alias: "Reference Date" },
-				{ name: "ResponseDate", type: "esriFieldTypeDate", alias: "Response Date" },
-				{ name: "RealignmentDate", type: "esriFieldTypeDate", alias: "Realignment Date" },
-				{ name: "EndArm", type: "esriFieldTypeDouble", alias: "End ARM" },
-				{ name: "EndSrmp", type: "esriFieldTypeDouble", alias: "End SRMP" },
-				{ name: "EndBack", type: "esriFieldTypeSmallInteger", alias: "End Back" },
-				{ name: "EndReferenceDate", type: "esriFieldTypeDate", alias: "End Reference Date" },
-				{ name: "EndResponseDate", type: "esriFieldTypeDate", alias: "End Response Date" },
-				{ name: "EndRealignDate", type: "esriFieldTypeDate", alias: "End Realignment Date" },
-				{ name: "ArmCalcReturnCode", type: "esriFieldTypeInteger", alias: "ArmCalc Return Code" },
-				{ name: "ArmCalcEndReturnCode", type: "esriFieldTypeInteger", alias: "ArmCalc End Return Code" },
-				{ name: "ArmCalcReturnMessage", type: "esriFieldTypeString", alias: "ArmCalc Return Message" },
-				{ name: "ArmCalcEndReturnMessage", type: "esriFieldTypeString", alias: "ArmCalc End Return Message" },
-				{ name: "LocatingError", type: "esriFieldTypeString", alias: "Locating Error" },
-				//{ name: "RouteGeometry", type: "esriFieldTypeGeometry" },
-				//{ name: "EventPoint", type: "esriFieldTypeGeometry" },
-				{ name: "Distance", type: "esriFieldTypeDouble" },
-				{ name: "Angle", type: "esriFieldTypeDouble" },
-		];
+			var infoTemplate = new InfoTemplate("Route Location", graphicToHtml);
 
-		lineResultsLayer = new FeatureLayer({
-			featureSet: null,
-			layerDefinition: {
-				geometryType: "esriGeometryPolyline",
-				fields: elcFields
-			}
-		}, {
-			id: "ElcLineResults",
-			infoTemplate: infoTemplate
-		});
+			var elcFields = [
+					{ name: "Id", type: "esriFieldTypeInteger", alias: "ID" },
+					{ name: "Route", type: "esriFieldTypeString" },
+					{ name: "Decrease", type: "esriFieldTypeSmallInteger" },
+					{ name: "Arm", type: "esriFieldTypeDouble", alias: "ARM" },
+					{ name: "Srmp", type: "esriFieldTypeDouble", alias: "SRMP" },
+					{ name: "Back", type: "esriFieldTypeSmallInteger" },
+					{ name: "ReferenceDate", type: "esriFieldTypeDate", alias: "Reference Date" },
+					{ name: "ResponseDate", type: "esriFieldTypeDate", alias: "Response Date" },
+					{ name: "RealignmentDate", type: "esriFieldTypeDate", alias: "Realignment Date" },
+					{ name: "EndArm", type: "esriFieldTypeDouble", alias: "End ARM" },
+					{ name: "EndSrmp", type: "esriFieldTypeDouble", alias: "End SRMP" },
+					{ name: "EndBack", type: "esriFieldTypeSmallInteger", alias: "End Back" },
+					{ name: "EndReferenceDate", type: "esriFieldTypeDate", alias: "End Reference Date" },
+					{ name: "EndResponseDate", type: "esriFieldTypeDate", alias: "End Response Date" },
+					{ name: "EndRealignDate", type: "esriFieldTypeDate", alias: "End Realignment Date" },
+					{ name: "ArmCalcReturnCode", type: "esriFieldTypeInteger", alias: "ArmCalc Return Code" },
+					{ name: "ArmCalcEndReturnCode", type: "esriFieldTypeInteger", alias: "ArmCalc End Return Code" },
+					{ name: "ArmCalcReturnMessage", type: "esriFieldTypeString", alias: "ArmCalc Return Message" },
+					{ name: "ArmCalcEndReturnMessage", type: "esriFieldTypeString", alias: "ArmCalc End Return Message" },
+					{ name: "LocatingError", type: "esriFieldTypeString", alias: "Locating Error" },
+					//{ name: "RouteGeometry", type: "esriFieldTypeGeometry" },
+					//{ name: "EventPoint", type: "esriFieldTypeGeometry" },
+					{ name: "Distance", type: "esriFieldTypeDouble" },
+					{ name: "Angle", type: "esriFieldTypeDouble" },
+			];
 
-		pointResultsLayer = new FeatureLayer({
-			featureSet: null,
-			layerDefinition: {
-				geometryType: "esriGeometryPoint",
-				fields: elcFields
-			}
-		}, {
-			id: "ElcPointResults",
-			infoTemplate: infoTemplate
-		});
+			lineResultsLayer = new FeatureLayer({
+				featureSet: null,
+				layerDefinition: {
+					geometryType: "esriGeometryPolyline",
+					fields: elcFields
+				}
+			}, {
+				id: "ElcLineResults",
+				infoTemplate: infoTemplate
+			});
 
-		this._layers = {
-			pointResults: pointResultsLayer,
-			lineResults: lineResultsLayer
-		};
+			pointResultsLayer = new FeatureLayer({
+				featureSet: null,
+				layerDefinition: {
+					geometryType: "esriGeometryPoint",
+					fields: elcFields
+				}
+			}, {
+				id: "ElcPointResults",
+				infoTemplate: infoTemplate
+			});
 
-		var clearBtn = document.querySelector(".clear-graphics-button");
-		clearBtn.addEventListener("click", function () {
-			pointResultsLayer.clear();
-			lineResultsLayer.clear();
-		});
-	}
+			this._layers = {
+				pointResults: pointResultsLayer,
+				lineResults: lineResultsLayer
+			};
 
-	ArcGisBufferUI.prototype.setMap = function (map) {
-		map.addLayer(this._layers.lineResults);
-		map.addLayer(this._layers.pointResults);
-		this.map = map;
-	};
+			var clearBtn = document.querySelector(".clear-graphics-button");
+			clearBtn.addEventListener("click", function () {
+				pointResultsLayer.clear();
+				lineResultsLayer.clear();
+			});
+		},
+		setMap: function (map) {
+			map.addLayer(this._layers.lineResults);
+			map.addLayer(this._layers.pointResults);
+			this.map = map;
+		}
+	});
 
-	return ArcGisBufferUI;
+	return ArcGisElcUI;
 });
