b8bba33af45f76b5d281c54cb723e4a49a74cccb
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-09-15 13:15:55 -0700

    Converted ElcUI to TypeScript

---
+++ packages/elc-ui/index.d.ts
@@ -0,0 +1,10 @@
+declare module "dojo/text!./Templates/elc-ui-bootstrap.min.html" {
+    const text: string;
+    export = text;
+}
+
+declare module "dojo/text!./Templates/elc-ui.min.html" {
+    const text: string;
+    export = text;
+}
+
