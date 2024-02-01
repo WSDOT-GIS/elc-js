b05b67ad381013233a52c4bda518e15339d0cf6d
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2024-01-31 16:18:42 -0800

    docs: :memo: Added jsdoc comments for FormatError.

--- packages/wsdot-elc/src/FormatError.ts
+++ packages/wsdot-elc/src/FormatError.ts
@@ -2,8 +2,20 @@
  * An error that is thrown when a string is not in the expected format.
  */
 export default class FormatError extends Error {
+  /**
+   * The value that was not in the expected format.
+   */
   public value: string;
+  /**
+   * The expected format for the input value.
+   */
   public expectedFormat: RegExp | null;
+  /**
+   * Constructor for creating an instance of {@link FormatError}.
+   *
+   * @param {string} value - The value that was not in the expected format.
+   * @param {RegExp} [expectedFormat] - The expected format for the input value.
+   */
   constructor(value: string, expectedFormat?: RegExp) {
     let message = `Not in expected format: "${value}".`;
     if (expectedFormat) {
